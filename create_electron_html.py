76aad84cdab5b9e27e0014b7ad359614bcf3aee0
sjenkins5 <jenkins.seth@epa.gov>
2018-07-02 11:44:37 -0700

    Update python script to reference disc_app_electron

--- create_electron_html.py
+++ create_electron_html.py
@@ -1,7 +1,7 @@
 """
 create_electron_html.py
-
-exec(open('electron/create_electron_html.py').read())
+python manage.py shell
+exec(open('disc_app_electron/create_electron_html.py').read())
 """
 
 from django.template.loader import render_to_string
@@ -10,36 +10,37 @@ from shutil import copyfile
 from shutil import copy
 
 try:
-    os.makedirs('electron/hwbi_app')
-    os.makedirs('electron/static_qed/hwbi/disc/css')
-    os.makedirs('electron/static_qed/hwbi/disc/img')
-    os.makedirs('electron/static_qed/hwbi/disc/js')
-    os.makedirs('electron/templates_qed/hwbi/disc')
-    os.makedirs('electron/hwbi_app')
+    os.makedirs('disc_app_electron/hwbi_app')
+    os.makedirs('disc_app_electron/static_qed/hwbi/disc/css')
+    os.makedirs('disc_app_electron/static_qed/hwbi/disc/img')
+    os.makedirs('disc_app_electron/static_qed/hwbi/disc/js')
+    os.makedirs('disc_app_electron/templates_qed/hwbi/disc')
+    os.makedirs('disc_app_electron/hwbi_app')
 except OSError as e:
     if e.errno != errno.EEXIST:
         raise
 
-copyfile('static_qed/hwbi/disc/css/hwbi-disc-app.css', 'electron/static_qed/hwbi/disc/css/hwbi-disc-app.css')
-copyfile('static_qed/hwbi/disc/css/jquery-ui.min.css', 'electron/static_qed/hwbi/disc/css/jquery-ui.min.css')
+copyfile('static_qed/hwbi/disc/css/hwbi-disc-app.css', 'disc_app_electron/static_qed/hwbi/disc/css/hwbi-disc-app.css')
+copyfile('static_qed/hwbi/disc/css/jquery-ui.min.css', 'disc_app_electron/static_qed/hwbi/disc/css/jquery-ui.min.css')
 
-copyfile('static_qed/hwbi/disc/js/jquery-ui.min.js', 'electron/static_qed/hwbi/disc/js/jquery-ui.min.js')
-copyfile('static_qed/hwbi/disc/js/hwbi-disc-app.js', 'electron/static_qed/hwbi/disc/js/hwbi-disc-app.js')
+copyfile('static_qed/hwbi/disc/js/jquery-ui.min.js', 'disc_app_electron/static_qed/hwbi/disc/js/jquery-ui.min.js')
+copyfile('static_qed/hwbi/disc/js/hwbi-disc-app.js', 'disc_app_electron/static_qed/hwbi/disc/js/hwbi-disc-app.js')
+copyfile('static_qed/hwbi/disc/js/hwbi-disc-report-v2.js', 'disc_app_electron/static_qed/hwbi/disc/js/hwbi-disc-report-v2.js')
 
-copyfile('static_qed/hwbi/disc/img/connection_to_nature.jpg', 'electron/static_qed/hwbi/disc/img/connection_to_nature.jpg')
-copyfile('static_qed/hwbi/disc/img/cultural_fulfillment.jpg', 'electron/static_qed/hwbi/disc/img/cultural_fulfillment.jpg')
-copyfile('static_qed/hwbi/disc/img/domain_arrow.png', 'electron/static_qed/hwbi/disc/img/domain_arrow.png')
-copyfile('static_qed/hwbi/disc/img/domain_bar.png', 'electron/static_qed/hwbi/disc/img/domain_bar.png')
-copyfile('static_qed/hwbi/disc/img/education.jpg', 'electron/static_qed/hwbi/disc/img/education.jpg')
-copyfile('static_qed/hwbi/disc/img/health.jpg', 'electron/static_qed/hwbi/disc/img/health.jpg')
-copyfile('static_qed/hwbi/disc/img/leisure_time.png', 'electron/static_qed/hwbi/disc/img/leisure_time.png')
-copyfile('static_qed/hwbi/disc/img/living_standards.jpg', 'electron/static_qed/hwbi/disc/img/living_standards.jpg')
-copyfile('static_qed/hwbi/disc/img/safety.jpg', 'electron/static_qed/hwbi/disc/img/safety.jpg')
-copyfile('static_qed/hwbi/disc/img/social_cohesion.jpg', 'electron/static_qed/hwbi/disc/img/social_cohesion.jpg')
-copyfile('static_qed/hwbi/disc/img/loader.gif', 'electron/static_qed/hwbi/disc/img/loader.gif')
-copyfile('static_qed/hwbi/disc/img/rainbow_need_to_replace.png', 'electron/static_qed/hwbi/disc/img/rainbow_need_to_replace.png')
+copyfile('static_qed/hwbi/disc/img/connection_to_nature.jpg', 'disc_app_electron/static_qed/hwbi/disc/img/connection_to_nature.jpg')
+copyfile('static_qed/hwbi/disc/img/cultural_fulfillment.jpg', 'disc_app_electron/static_qed/hwbi/disc/img/cultural_fulfillment.jpg')
+copyfile('static_qed/hwbi/disc/img/domain_arrow.png', 'disc_app_electron/static_qed/hwbi/disc/img/domain_arrow.png')
+copyfile('static_qed/hwbi/disc/img/domain_bar.png', 'disc_app_electron/static_qed/hwbi/disc/img/domain_bar.png')
+copyfile('static_qed/hwbi/disc/img/education.jpg', 'disc_app_electron/static_qed/hwbi/disc/img/education.jpg')
+copyfile('static_qed/hwbi/disc/img/health.jpg', 'disc_app_electron/static_qed/hwbi/disc/img/health.jpg')
+copyfile('static_qed/hwbi/disc/img/leisure_time.png', 'disc_app_electron/static_qed/hwbi/disc/img/leisure_time.png')
+copyfile('static_qed/hwbi/disc/img/living_standards.jpg', 'disc_app_electron/static_qed/hwbi/disc/img/living_standards.jpg')
+copyfile('static_qed/hwbi/disc/img/safety.jpg', 'disc_app_electron/static_qed/hwbi/disc/img/safety.jpg')
+copyfile('static_qed/hwbi/disc/img/social_cohesion.jpg', 'disc_app_electron/static_qed/hwbi/disc/img/social_cohesion.jpg')
+copyfile('static_qed/hwbi/disc/img/loader.gif', 'disc_app_electron/static_qed/hwbi/disc/img/loader.gif')
+copyfile('static_qed/hwbi/disc/img/rainbow_need_to_replace.png', 'disc_app_electron/static_qed/hwbi/disc/img/rainbow_need_to_replace.png')
 
-copyfile('hwbi_app/hwbi_db.sqlite3', 'electron/hwbi_app/hwbi_db.sqlite3')
+copyfile('hwbi_app/hwbi_db.sqlite3', 'disc_app_electron/hwbi_app/hwbi_db.sqlite3')
 
 
 temp_google_key = 'AIzaSyDEC5r_Tq31qfF8BKIdhUAH1KorOfjLV4g'
@@ -64,7 +65,7 @@ html += render_to_string('disc/drupal_2017/10epa_drupal_footer.html', {
     'electron': electron
 })
 #print(html)
-#f = open('electron/templates_qed/hwbi/disc/index.html', 'w')
-f = open('electron/index.html', 'w')
+#f = open('disc_app_electron/templates_qed/hwbi/disc/index.html', 'w')
+f = open('disc_app_electron/index.html', 'w')
 f.write(html)
 f.closed
