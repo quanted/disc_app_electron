712a92482a5b2c12155e8456b355e92adef270a1
Seth Jenkins <jenkins.seth@epa.gov>
2019-09-12 10:23:29 -0700

    remove unnecessary Platform variable

--- buildScript.js
+++ buildScript.js
@@ -1,32 +1,29 @@
 "use strict";
 
 const builder = require("electron-builder");
-const Platform = builder.Platform;
 
 const config = {
-  "asar": true,
-  "appId": "disc.app",
-  "productName": "Decision Integration for Strong Communities",
-  "mac": {
-    "category": "disc.app"
+  asar: true,
+  appId: "disc.app",
+  productName: "Decision Integration for Strong Communities",
+  mac: {
+    category: "disc.app"
   },
-  "nsis": {
-    "oneClick": false
+  nsis: {
+    oneClick: false
   },
-  "extraResources": [
-    "hwbi_app/DISC.db",
-    "hwbi_app/cities.db",
-  ]
+  extraResources: ["hwbi_app/DISC.db", "hwbi_app/cities.db"]
 };
 
-builder.build({
+builder
+  .build({
     win: ["default"],
     mac: ["default"],
     config: config
-})
-.then(m => {
-    console.log('Build OK!');
-})
-.catch(e => {
+  })
+  .then(m => {
+    console.log("Build OK!");
+  })
+  .catch(e => {
     console.error(e);
-});
+  });
