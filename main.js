3f0f13ea7a9e468e0ffed1478102e8644e3bf69e
Seth Jenkins <jenkins.seth@epa.gov>
2019-10-15 10:15:40 -0700

    update electron version; remove electron-pdf-window module

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const path = require("path");
 const url = require("url");
 const Papa = require("papaparse");
-const PDFWindow = require("electron-pdf-window");
 
 // SET ENV
 //process.env.NODE_ENV = 'production';
